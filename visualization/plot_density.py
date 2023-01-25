4ae2624f60bdab22999047a40d30ea7d7ab1ae35
newmanno <newmanno@oregonstate.edu>
2023-01-25 13:43:50 -0800

    Added error handling for small/dense networks

--- visualization/plot_density.py
+++ visualization/plot_density.py
@@ -10,6 +10,7 @@ import pandas as pd
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
+from functools import partial
 import numpy as np
 
 if __name__ == '__main__':
@@ -41,17 +42,33 @@ if __name__ == '__main__':
             plt.annotate(k + "\n" + dic_prob[k], (v[1]+0.01,v[0]), fontsize=18)
         return(plt)
     
+    def check_rand_net_output(df):
+        '''
+        Function that determines whether a given data frame is suitable for density plotting based on its range of degrees
         
+        There is a limitation to the code, where matplotlib will throw an error if the range in degrees is not at least 2,
+        so this function checks if that is the case
+        
+        Arguments:
+            - df: the random network data frame, pre-filtered to remove any N/As that may have been present
+        '''
+        max_degree = max(df['Degree'])
+        min_degree = min(df['Degree'])
+        range_degree = max_degree - min_degree
+        
+        if range_degree < 2:
+            return(False)
+        else:
+            return(True)
     
     
     
     
     
-    
-    testing = False
+    testing = True
     
     if testing:
-        rand_net = "chol_random_network.csv"
+        rand_net = "synthesized_stats (1).csv"
         pick = "inputs_for_downstream_plots.pickle"
         # nnodes_density = 4
         # nnodes_density_per_type = 3
@@ -88,116 +105,140 @@ if __name__ == '__main__':
         # node_props = args.density_node_types
         nodes_to_plot = args.nodes_to_plot
 
-    ##############################################################
-    # Plot random network density for top nodes from the dotplots
-    ##############################################################
-        
+
+    ########################
+    # Import data
+    ########################
     rand_nets = pd.read_csv(rand_net, header = 0)
     rand_nets = rand_nets.iloc[:,2:] 
     
-    # Load the node_props pickle file that already contains the data type
-    p = open(pick, "rb")
-    pload = pickle.load(p)
-    node_props = pload[0]
-    top_nodes = pload[1]
-    top_nodes_per_type = pload[2]
+    # drop any N/A values from the dataframe, which would be present if its a small 
+    # and dense network where all nodes of one BiBC group connect to all nodes of the other group
+    rand_nets = rand_nets.dropna(axis=0) 
+    rand_nets_check = check_rand_net_output(rand_nets)
     
-    # First find how many top nodes have both a degree and BiBC greater than the indicated node
-    all_nodes_dict = {}
     
-    BiBC_deg_sorted = node_props.sort_values(['BiBC', 'Node_degrees'], ascending=[False, False])
-    sorted_node_props_cut = BiBC_deg_sorted[['index','Node_degrees', 'BiBC']]
-    sorted_node_props_cut_top = sorted_node_props_cut[sorted_node_props_cut['index'].isin(top_nodes)]
-    
-    # populate dictionary so you get node degrees and BiBC as values for each key (node)
-    for index, row in sorted_node_props_cut.iterrows():
-        all_nodes_dict[row[0]] = [row[1], row[2]]
- 
-    prob_dict = {} # Dictionary for plotting probabilities of nodes
-    
-    sorted_node_props_top_list = sorted_node_props_cut_top['index'].tolist()
-    
-    # Calculate probabilities for all nodes, where chance to find a node randomly is the "square" that can be drawn on the density plot, 
-    # meaning it counts the number of nodes found randomly in networks that have both a greater degree and BiBC than each calculated node
-    with open("./plots/probabilities_to_randomly_find_nodes.csv", "w") as file:
-
-        file.write("Node,Observed degree, Observed BiBC, Probability\n")
+    # If the random network data frame passes the check:
+    if rand_nets_check:
         
-        for node in BiBC_deg_sorted['index'].tolist():
-            greater_than_density_pval_node = rand_nets[(rand_nets['Degree'] > all_nodes_dict[node][0]) & (rand_nets['BiBC'] > all_nodes_dict[node][1])]
-            rand_net_pval = round(len(greater_than_density_pval_node)/len(rand_nets) * 100, 2) 
-            prob_dict[node] = str(rand_net_pval) + "%"
-            file.write(node + "," + str(all_nodes_dict[node][0]) + "," + str(all_nodes_dict[node][1]) + "," + str(rand_net_pval) + "%\n")
+        ##############################################################
+        # Plot random network density for top nodes from the dotplots
+        ##############################################################    
+        # Load the node_props pickle file that already contains the data type
+        p = open(pick, "rb")
+        pload = pickle.load(p)
+        node_props = pload[0]
+        top_nodes = pload[1]
+        top_nodes_per_type = pload[2]
+        
+        # First find how many top nodes have both a degree and BiBC greater than the indicated node
+        all_nodes_dict = {}
+        
+        BiBC_deg_sorted = node_props.sort_values(['BiBC', 'Node_degrees'], ascending=[False, False])
+        sorted_node_props_cut = BiBC_deg_sorted[['index','Node_degrees', 'BiBC']]
+        sorted_node_props_cut_top = sorted_node_props_cut[sorted_node_props_cut['index'].isin(top_nodes)]
+        
+        # populate dictionary so you get node degrees and BiBC as values for each key (node)
+        for index, row in sorted_node_props_cut.iterrows():
+            all_nodes_dict[row[0]] = [row[1], row[2]]
+     
+        prob_dict = {} # Dictionary for plotting probabilities of nodes
+        
+        sorted_node_props_top_list = sorted_node_props_cut_top['index'].tolist()
+        
+        # Calculate probabilities for all nodes, where chance to find a node randomly is the "square" that can be drawn on the density plot, 
+        # meaning it counts the number of nodes found randomly in networks that have both a greater degree and BiBC than each calculated node
+        with open("./plots/probabilities_to_randomly_find_nodes.csv", "w") as file:
     
-    top_nodes_dict = {}
-    
-    # list_nodes = sorted_node_props_cut_top['index'].tolist()
-    sorted_node_props_top_cut = sorted_node_props_cut_top[['index','Node_degrees', 'BiBC']]
+            file.write("Node,Observed degree, Observed BiBC, Probability\n")
             
-    # Find top nodes to plot (same nodes as in zoomed in Degree-BiBC plot)
-    for index, row in sorted_node_props_top_cut.iterrows():
-        top_nodes_dict[row[0]] = [row[1], row[2]]
-
-    kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
-    kdefig = add_text(top_nodes_dict, prob_dict)
-
-    #kdefig = kdefig.get_figure()
-    kdefig.savefig("./plots/density_plot_with_top_nodes_from_dotplots.png", bbox_inches='tight')
-    print("Plot saved: density_plot_with_top_nodes_from_dotplots.png")
-    kdefig.clf()
-    
-    
-   
-    
-    ###############################################################
-    # Plot random network density for top nodes per data type
-    ###############################################################
-    
-    # Only plot the top X nodes, where X is a user-defined number
-    for k,v in top_nodes_per_type.items():
-        # print(k,v)
+            for node in BiBC_deg_sorted['index'].tolist():
+                greater_than_density_pval_node = rand_nets[(rand_nets['Degree'] > all_nodes_dict[node][0]) & (rand_nets['BiBC'] > all_nodes_dict[node][1])]
+                rand_net_pval = round(len(greater_than_density_pval_node)/len(rand_nets) * 100, 2) 
+                prob_dict[node] = str(rand_net_pval) + "%"
+                file.write(node + "," + str(all_nodes_dict[node][0]) + "," + str(all_nodes_dict[node][1]) + "," + str(rand_net_pval) + "%\n")
         
-        top_dens_nodes_per_type = BiBC_deg_sorted[BiBC_deg_sorted['index'].isin(v)]
-        top_dens_nodes_per_type = top_dens_nodes_per_type[['index','Node_degrees', 'BiBC']]
-       
-        # Convert df to dicctionary to plot
-        top_dens_nodes_per_type_dict = {} 
-        for index, row in top_dens_nodes_per_type.iterrows():
-            top_dens_nodes_per_type_dict[row[0]] = [row[1], row[2]]
+        top_nodes_dict = {}
         
-        kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')    
-        kdefig = add_text(top_dens_nodes_per_type_dict, prob_dict)
-        
-        kdefig.savefig("./plots/density_plot_with_top_" + k + "_nodes_only.png", bbox_inches='tight')
-        print("Plot saved: density_plot_with_top_" + k + "_nodes_only.png")
-        kdefig.clf()
-
+        # list_nodes = sorted_node_props_cut_top['index'].tolist()
+        sorted_node_props_top_cut = sorted_node_props_cut_top[['index','Node_degrees', 'BiBC']]
+                
+        # Find top nodes to plot (same nodes as in zoomed in Degree-BiBC plot)
+        for index, row in sorted_node_props_top_cut.iterrows():
+            top_nodes_dict[row[0]] = [row[1], row[2]]
     
     
     
-    ###############################################################
-    # Plot random network density for user-defined list of nodes
-    ###############################################################
-        
-    if nodes_to_plot:
-        # Check that the nodes the user wants are in the df
-        if set(nodes_to_plot).issubset(BiBC_deg_sorted['index'].tolist()) == False: 
-            raise Exception("ERROR: Please make sure the desired nodes are in the node properties file.")
-        
-        user_selected_nodes_dict = {} # dictionary to hold user-specified genes, degree, and bibc
-    
-        # Only plot the user-specified nodes from dens_list_to_plot
-        for i in nodes_to_plot:
-            print(i)
-            selected_node = BiBC_deg_sorted[BiBC_deg_sorted['index']  == i]
-            selected_node = selected_node[['index','Node_degrees', 'BiBC']]
-            
-            for index, row in selected_node.iterrows():
-                user_selected_nodes_dict[row[0]] = [row[1], row[2]]
-        
         kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
-        kdefig = add_text(user_selected_nodes_dict, prob_dict)
-        
-        kdefig.savefig("./plots/density_plot_with_user_defined_node_list.png", bbox_inches='tight')
-        print("Plot saved: density_plot_with_user_defined_node_list.png")
+        kdefig = add_text(top_nodes_dict, prob_dict)
+    
+        #kdefig = kdefig.get_figure()
+        kdefig.savefig("./plots/density_plot_with_top_nodes_from_dotplots.png", bbox_inches='tight')
+        print("Plot saved: density_plot_with_top_nodes_from_dotplots.png")
         kdefig.clf()
+        
+        
+       
+        
+        ###############################################################
+        # Plot random network density for top nodes per data type
+        ###############################################################
+        
+        # Only plot the top X nodes, where X is a user-defined number
+        for k,v in top_nodes_per_type.items():
+            # print(k,v)
+            
+            top_dens_nodes_per_type = BiBC_deg_sorted[BiBC_deg_sorted['index'].isin(v)]
+            top_dens_nodes_per_type = top_dens_nodes_per_type[['index','Node_degrees', 'BiBC']]
+           
+            # Convert df to dicctionary to plot
+            top_dens_nodes_per_type_dict = {} 
+            for index, row in top_dens_nodes_per_type.iterrows():
+                top_dens_nodes_per_type_dict[row[0]] = [row[1], row[2]]
+            
+            kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')    
+            kdefig = add_text(top_dens_nodes_per_type_dict, prob_dict)
+            
+            kdefig.savefig("./plots/density_plot_with_top_" + k + "_nodes_only.png", bbox_inches='tight')
+            print("Plot saved: density_plot_with_top_" + k + "_nodes_only.png")
+            kdefig.clf()
+    
+        
+        
+        
+        ###############################################################
+        # Plot random network density for user-defined list of nodes
+        ###############################################################
+            
+        if nodes_to_plot:
+            # Check that the nodes the user wants are in the df
+            if set(nodes_to_plot).issubset(BiBC_deg_sorted['index'].tolist()) == False: 
+                raise Exception("ERROR: Please make sure the desired nodes are in the node properties file.")
+            
+            user_selected_nodes_dict = {} # dictionary to hold user-specified genes, degree, and bibc
+        
+            # Only plot the user-specified nodes from dens_list_to_plot
+            for i in nodes_to_plot:
+                # print(i)
+                selected_node = BiBC_deg_sorted[BiBC_deg_sorted['index']  == i]
+                selected_node = selected_node[['index','Node_degrees', 'BiBC']]
+                
+                for index, row in selected_node.iterrows():
+                    user_selected_nodes_dict[row[0]] = [row[1], row[2]]
+            
+            kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
+            kdefig = add_text(user_selected_nodes_dict, prob_dict)
+            
+            kdefig.savefig("./plots/density_plot_with_user_defined_node_list.png", bbox_inches='tight')
+            print("Plot saved: density_plot_with_user_defined_node_list.png")
+            kdefig.clf()
+
+    # If network is too small/too dense, don't create figure
+    else:
+        raise Exception("\nError: random network density cannot be plotted. This is likely because you have too small and too dense of a network. This error will occur if the range of degrees in " + rand_net + " is less than 2, meaning all nodes have too similar of a degree.\n")
+        
+        
+        
+        
+        
+        
