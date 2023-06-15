8a61c58e1f192fae07d203e2d05a0377195f7788
newmanno <newmanno@oregonstate.edu>
2023-06-14 17:09:16 -0700

    Labels on plot no longer overlap

--- visualization/plot_density.py
+++ visualization/plot_density.py
@@ -2,7 +2,7 @@
 """
 Created on Tue Jan 17 09:59:18 2023
 
-@author: Nolan
+@author: Nolan K Newman <newmanno@oregonstate.edu>
 """
 
 import argparse
@@ -10,10 +10,22 @@ import pandas as pd
 import pickle
 import seaborn as sns
 import matplotlib.pyplot as plt
+from matplotlib.ticker import Formatter
+import matplotlib as mpl
 from functools import partial
 import numpy as np
+from adjustText import adjust_text
+from mpl_toolkits.axes_grid1.inset_locator import inset_axes
+import sys
+
+class LowHighFormatter(Formatter):
+    def format_data(self, value):
+        return "Low" if value == 0 else "High"
+    def format_ticks(self, values):
+        return [self.format_data(value) for value in values]
 
 if __name__ == '__main__':
+
 
     def plot_kde(xprop, yprop, col):
         '''
@@ -25,11 +37,37 @@ if __name__ == '__main__':
             - col: name of color palette to use
         '''
         sns.set(font_scale=1.5)
+        plt.figure(figsize=(10,8))
 
-        fig = sns.kdeplot(xprop,yprop,shade=True,cmap=col, cbar=True, cbar_kws=dict(ticks=[], label='Likelihood of random finding'))
-        return(fig)
+        ax = plt.axes()
+        #cbaxes = inset_axes(ax, width="100%", height="10%", loc=8)
+        cax, kw = mpl.colorbar.make_axes(ax, location="top", orientation="horizontal", pad=0.075)
 
-    def add_text(dic_obs, dic_prob):
+        # The following works but cbar labels are on top
+        fig1 = sns.kdeplot(xprop, yprop, shade=True, ax=ax, cmap=col, cbar=True, cbar_ax=cax, cbar_kws=dict(ticks=[0,9], format=LowHighFormatter(), label='Likelihood of random finding', use_gridspec=False, **kw))
+
+        cax.xaxis.set_ticks_position('bottom')
+
+
+        #fig1 = sns.kdeplot(xprop, yprop, shade=True, cmap=col)
+        #cb = mpl.colorbar.ColorbarBase(fig1, ticks=[0,9], format=LowHighFormatter(), label='Likelihood of random finding', orientation='horizontal')
+        #fig1.xaxis.set_ticks_position('top')
+
+        #fig1 = sns.kdeplot(xprop,yprop,shade=True,cmap=col, cbar=True, cbar_kws=dict(ticks = [0,9], label='Likelihood of random finding', use_gridspec = False, location = "top"))
+        #fig2 = sns.kdeplot(xprop,yprop,shade=True)
+
+
+        #cbar = fig1.collections[0].colorbar
+        #print(fig1)
+
+        #cbar.set_ticklabels(['Low', 'High'])
+
+        #plt.draw()
+
+
+        return(fig1)
+
+    def add_text(dic_obs, dic_prob, ax):
         '''
         Function that adds text to kde plot from a dictionary
 
@@ -37,10 +75,33 @@ if __name__ == '__main__':
             - dic_obs: dictionary keyed on node name that contains a list of the observed Degree and BiBC for each node as values
             - dic_prob: dictionary keyed on node name that contains the likelihood of finding that node randomly as values
         '''
+        dots = []
+        texts_bibcdeg = []
+        bboxes = []
+
         for k,v in dic_obs.items():
-            plt.plot(v[1],v[0], "ko", zorder = 10)
-            plt.annotate(k + "\n" + dic_prob[k], (v[1]+0.01,v[0]), fontsize=8)
-        return(plt)
+            dot = ax.plot(v[1],v[0], "ko", zorder = 10)[0]
+            dots.extend(ax.plot(v[1],v[0], "ko", zorder = 10))
+
+            texts_bibcdeg.append(ax.text(v[1], v[0], k + "\n" + dic_prob[k], ha='center', va='center', fontsize=12))
+            bbox = dot.get_window_extent(ax.get_figure().canvas.get_renderer())
+            #bb_height_mult = 10000 * bbox.height
+            #bb_width_mult = 10000 * bbox.width
+            #bbox = mpl.transforms.Bbox([[bbox.xmin-bb_width_mult, bbox.ymin-bb_height_mult], [bbox.xmax+bb_width_mult, bbox.ymax+bb_height_mult]])
+            bbox = bbox.expanded(1.1, 1.1)
+            bboxes.append(bbox)
+
+        #print(bboxes)
+        bkgnd_cols = mpl.cm.get_cmap('Reds')
+        bkgnd_col = bkgnd_cols(13)
+        print(bkgnd_col)
+
+        ax.set_facecolor(bkgnd_col)
+        ax.grid(False)
+        adjust_text(texts_bibcdeg, ax=ax, objects = bboxes, time_lim=3)
+
+
+        return(texts_bibcdeg)
 
     def check_rand_net_output(df):
         '''
@@ -64,16 +125,16 @@ if __name__ == '__main__':
 
 
 
-
     testing = False
 
     if testing:
-        rand_net = "synthesized_stats (1).csv"
-        pick = "inputs_for_downstream_plots.pickle"
+        rand_net = "random_networks_synthesized_Nolan.csv"
+        pick = "inputs_for_downstream_plots_Nolan.pickle"
         # nnodes_density = 4
         # nnodes_density_per_type = 3
         # density_node_types = ['gene']
-        nodes_to_plot = ['geneH', 'geneF']
+        bibc_name = "BiBC"
+        nodes_to_plot = ['OTU_259506', 'fasting leptin']
 
     else:
         print("")
@@ -82,6 +143,7 @@ if __name__ == '__main__':
         requiredArgGroup  = parser.add_argument_group('Required arguments')
         requiredArgGroup.add_argument("--rand-net", type=str, dest="rand_net", help="Random network file output by synthesize_network_stats.py", required=True)
         requiredArgGroup.add_argument("--pickle", type=str, help="inputs_for_downstream_plots.pickle file output by dot_plots.py", required=True)
+        requiredArgGroup.add_argument("--bibc-name", type=str, dest="bibc_name", help="The 'name' of the BiBC calculation performed, from the node_properties.txt file. Example: BiBC_microbe_pheno (if BiBC was calculated between the microbe and pheno groups)", required=True)
 
         optionalArgGroup  = parser.add_argument_group('Optional arguments')
         optionalArgGroup.add_argument("-h", "--help", action="help", help="Show this help message and exit")
@@ -103,6 +165,7 @@ if __name__ == '__main__':
         # nnodes_density = args.nnodes_density
         # nnodes_density_per_type = args.nnodes_density_per_type
         # node_props = args.density_node_types
+        bibc_name = args.bibc_name
         nodes_to_plot = args.nodes_to_plot
 
 
@@ -117,6 +180,7 @@ if __name__ == '__main__':
     rand_nets = rand_nets.dropna(axis=0)
     rand_nets_check = check_rand_net_output(rand_nets)
 
+    print(rand_nets)
 
     # If the random network data frame passes the check:
     if rand_nets_check:
@@ -130,14 +194,18 @@ if __name__ == '__main__':
         node_props = pload[0]
         top_nodes = pload[1]
         top_nodes_per_type = pload[2]
-        plotdir = pload[3] # location of previously created plots directory
+        plotdir = "./" # location of previously created plots directory
 
+        # !!!!!!!!!!Must uncomment the following before publishing
+        # plotdir = pload[3] # location of previously created plots directory
+
+        print(node_props)
 
         # First find how many top nodes have both a degree and BiBC greater than the indicated node
         all_nodes_dict = {}
 
-        BiBC_deg_sorted = node_props.sort_values(['BiBC', 'Node_degrees'], ascending=[False, False])
-        sorted_node_props_cut = BiBC_deg_sorted[['index','Node_degrees', 'BiBC']]
+        BiBC_deg_sorted = node_props.sort_values([bibc_name, 'Node_degrees'], ascending=[False, False])
+        sorted_node_props_cut = BiBC_deg_sorted[['index','Node_degrees', bibc_name]]
         sorted_node_props_cut_top = sorted_node_props_cut[sorted_node_props_cut['index'].isin(top_nodes)]
 
         # populate dictionary so you get node degrees and BiBC as values for each key (node)
@@ -163,7 +231,7 @@ if __name__ == '__main__':
         top_nodes_dict = {}
 
         # list_nodes = sorted_node_props_cut_top['index'].tolist()
-        sorted_node_props_top_cut = sorted_node_props_cut_top[['index','Node_degrees', 'BiBC']]
+        sorted_node_props_top_cut = sorted_node_props_cut_top[['index','Node_degrees', bibc_name]]
 
         # Find top nodes to plot (same nodes as in zoomed in Degree-BiBC plot)
         for index, row in sorted_node_props_top_cut.iterrows():
@@ -172,12 +240,13 @@ if __name__ == '__main__':
 
 
         kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
-        kdefig = add_text(top_nodes_dict, prob_dict)
+        texts = add_text(top_nodes_dict, prob_dict, kdefig)
+
 
         #kdefig = kdefig.get_figure()
-        kdefig.savefig(plotdir + "density_plot_with_top_nodes_from_dotplots.png", bbox_inches='tight')
+        plt.savefig(plotdir + "density_plot_with_top_nodes_from_dotplots.png", bbox_inches='tight', dpi=600)
         print("Plot saved: " + plotdir + "density_plot_with_top_nodes_from_dotplots.png")
-        kdefig.clf()
+        plt.clf()
 
 
 
@@ -191,7 +260,7 @@ if __name__ == '__main__':
             # print(k,v)
 
             top_dens_nodes_per_type = BiBC_deg_sorted[BiBC_deg_sorted['index'].isin(v)]
-            top_dens_nodes_per_type = top_dens_nodes_per_type[['index','Node_degrees', 'BiBC']]
+            top_dens_nodes_per_type = top_dens_nodes_per_type[['index','Node_degrees', bibc_name]]
 
             # Convert df to dictionary to plot
             top_dens_nodes_per_type_dict = {}
@@ -199,11 +268,11 @@ if __name__ == '__main__':
                 top_dens_nodes_per_type_dict[row[0]] = [row[1], row[2]]
 
             kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
-            kdefig = add_text(top_dens_nodes_per_type_dict, prob_dict)
+            kdefig = add_text(top_dens_nodes_per_type_dict, prob_dict, kdefig)
 
-            kdefig.savefig(plotdir + "density_plot_with_top_" + k + "_nodes_only.png", bbox_inches='tight')
+            plt.savefig(plotdir + "density_plot_with_top_" + k + "_nodes_only.png", bbox_inches='tight', dpi=600)
             print("Plot saved: " + plotdir + "density_plot_with_top_" + k + "_nodes_only.png")
-            kdefig.clf()
+            plt.clf()
 
 
 
@@ -223,17 +292,17 @@ if __name__ == '__main__':
             for i in nodes_to_plot:
                 # print(i)
                 selected_node = BiBC_deg_sorted[BiBC_deg_sorted['index']  == i]
-                selected_node = selected_node[['index','Node_degrees', 'BiBC']]
+                selected_node = selected_node[['index','Node_degrees', bibc_name]]
 
                 for index, row in selected_node.iterrows():
                     user_selected_nodes_dict[row[0]] = [row[1], row[2]]
 
             kdefig = plot_kde(rand_nets['BiBC'], rand_nets['Degree'], 'Reds')
-            kdefig = add_text(user_selected_nodes_dict, prob_dict)
+            kdefig = add_text(user_selected_nodes_dict, prob_dict, kdefig)
 
-            kdefig.savefig(plotdir + "density_plot_with_user_defined_node_list.png", bbox_inches='tight')
+            plt.savefig(plotdir + "density_plot_with_user_defined_node_list.png", bbox_inches='tight', dpi=600)
             print("Plot saved: " + plotdir + "density_plot_with_user_defined_node_list.png")
-            kdefig.clf()
+            plt.clf()
 
     # If network is too small/too dense, don't create figure
     else:
